5e9ae991dd63a32dfc6043d3f7e22502e9fdb9c1
chenlm <chenlm@huilan.com>
2016-02-19 16:37:55 +0800

    test framework ok

--- examples/qwf-school-mis/qwf-school-mis-student/src/com/qwf/school/mis/student/controller/StudentEditController.java
+++ examples/qwf-school-mis/qwf-school-mis-student/src/com/qwf/school/mis/student/controller/StudentEditController.java
@@ -3,6 +3,7 @@ package com.qwf.school.mis.student.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.qwf.school.mis.student.entity.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,18 +13,22 @@ import com.qwf.school.mis.student.service.StudentService;
 
 @Controller
 public class StudentEditController {
-	@Autowired
-	private StudentService studentService;
+    @Autowired
+    private StudentService studentService;
 
-	@RequestMapping(value = "edit", method = RequestMethod.GET)
-	public String get_edit(HttpServletRequest request,
-			HttpServletResponse response, String id) {
-		return "edit";
-	}
+    @RequestMapping(value = "edit", method = RequestMethod.GET)
+    public String get_edit(HttpServletRequest request,
+                           HttpServletResponse response, String id) {
+        return "edit";
+    }
 
-	@RequestMapping(value = "test", method = RequestMethod.GET)
-	public String get_test(HttpServletRequest request,
-						   HttpServletResponse response, String id) {
-		return "test";
-	}
+    @RequestMapping(value = "edit", method = RequestMethod.POST)
+    public String get_test(HttpServletRequest request,
+                           HttpServletResponse response, String id) {
+        final Student stu = new Student();
+        stu.setId("123");
+        stu.setName("chenlm");
+        studentService.addStudent(stu);
+        return "edit";
+    }
 }
